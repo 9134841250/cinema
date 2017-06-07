0eafe88a01cd6ea0cede02c70ce2ad464dc91f66
huhonok <huhonok@gmail.com>
2017-06-07 07:31:38 +0700

    add module for adding values to db

--- server/src/main/java/ru/nstu/cinema/server/CinemaStorage.java
+++ server/src/main/java/ru/nstu/cinema/server/CinemaStorage.java
@@ -5,10 +5,11 @@ import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 import ru.nstu.cinema.common.entity.Film;
 import ru.nstu.cinema.common.entity.Hall;
-import ru.nstu.cinema.common.entity.Seat;
 import ru.nstu.cinema.common.entity.Session;
 
+import java.security.*;
 import java.sql.*;
+import java.sql.Timestamp;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -37,7 +38,12 @@ public class CinemaStorage {
         return (JSONObject)parser.parse(str);
     }
 
-    public void addHall(Hall hall){
+    /***
+     * Добавить зал в БД
+     * @param hall объект описания зала кинотеатра
+     * @return
+     */
+    public Hall addHall(Hall hall){
         Connection conn = null;
         PreparedStatement stmt = null;
         try{
@@ -57,13 +63,20 @@ public class CinemaStorage {
                     hallFields.name,
                     hallFields.descr,
                     hallFields.struct);
-            stmt = conn.prepareStatement(sql);
+            stmt = conn.prepareStatement(sql,Statement.RETURN_GENERATED_KEYS);
             stmt.setString(1, hall.getName());
             stmt.setString(2, hall.getDescription());
             stmt.setString(3, hall.getStructure().toString());
-            int rs = stmt.executeUpdate(sql);
+            stmt.execute();
 
-
+            try (ResultSet generatedKeys = stmt.getGeneratedKeys()) {
+            if (generatedKeys.next()) {
+                hall.setId(generatedKeys.getInt(1));
+            }
+            else {
+                throw new SQLException("Creating user failed, no ID obtained.");
+            }
+        }
             stmt.close();
             conn.close();
         } catch(Exception e){
@@ -84,19 +97,24 @@ public class CinemaStorage {
             }//end finally try
         }//end try
         System.out.println("Goodbye!");
-
+        return hall;
     }
 
-    public void addFilm(Film film){
+    /***
+     * Добавить фильм
+     * @param film объект описания фильма
+     * @return
+     */
+    public Film addFilm(Film film) {
         Connection conn = null;
         PreparedStatement stmt = null;
-        try{
+        try {
             //STEP 2: Register JDBC driver
             Class.forName(JDBC_DRIVER);
 
             //STEP 3: Open a connection
             System.out.println("Connecting to database...");
-            conn = DriverManager.getConnection(DB_URL,USER,PASS);
+            conn = DriverManager.getConnection(DB_URL, USER, PASS);
 
             //STEP 4: Execute a query
             System.out.println("Creating statement...");
@@ -106,35 +124,46 @@ public class CinemaStorage {
                     databases.film,
                     filmFields.name,
                     filmFields.descr);
-            stmt = conn.prepareStatement(sql);
+            stmt = conn.prepareStatement(sql,Statement.RETURN_GENERATED_KEYS);
             stmt.setString(1, film.getName());
             stmt.setString(2, film.getDescr().toString());
-            int rs = stmt.executeUpdate(sql);
-
-
+            stmt.execute();
+            try (ResultSet generatedKeys = stmt.getGeneratedKeys()) {
+                if (generatedKeys.next()) {
+                    film.setId(generatedKeys.getInt(1));
+                } else {
+                    throw new SQLException("Creating user failed, no ID obtained.");
+                }
+            }
             stmt.close();
             conn.close();
-        } catch(Exception e){
+        } catch (Exception e) {
             //Handle errors for Class.forName
             e.printStackTrace();
-        }finally{
+        } finally {
             //finally block used to close resources
-            try{
-                if(stmt!=null)
+            try {
+                if (stmt != null)
                     stmt.close();
-            }catch(SQLException ignored){
+            } catch (SQLException ignored) {
             }// nothing we can do
-            try{
-                if(conn!=null)
+            try {
+                if (conn != null)
                     conn.close();
-            }catch(SQLException se){
+            } catch (SQLException se) {
                 se.printStackTrace();
             }//end finally try
         }//end try
         System.out.println("Goodbye!");
-
+        return film;
     }
-      public void addSession(Session session){
+
+    /***
+     * Добавить сеанс в БД
+     * @param session
+     * @return
+     */
+      public Session addSession(Session session){
         Connection conn = null;
         PreparedStatement stmt = null;
         try{
@@ -156,13 +185,20 @@ public class CinemaStorage {
                     sessionFields.film_id,
                     sessionFields.price
                     );
-            stmt = conn.prepareStatement(sql);
-            stmt.setDate(1, Date.valueOf(session.getTime().toLocalDate()));
+            stmt = conn.prepareStatement(sql,Statement.RETURN_GENERATED_KEYS);
+            Timestamp timestamp =Timestamp.valueOf(session.getTime());
+            stmt.setTimestamp(1, timestamp);
             stmt.setInt(2, session.getHall().getId());
             stmt.setInt(3, session.getFilm().getId());
             stmt.setInt(4, session.getPrice());
-            int rs = stmt.executeUpdate(sql);
-
+            stmt.execute();
+            try (ResultSet generatedKeys = stmt.getGeneratedKeys()) {
+                if (generatedKeys.next()) {
+                    session.setId(generatedKeys.getInt(1));
+                } else {
+                    throw new SQLException("Creating user failed, no ID obtained.");
+                }
+            }
 
             stmt.close();
             conn.close();
@@ -184,10 +220,71 @@ public class CinemaStorage {
             }//end finally try
         }//end try
         System.out.println("Goodbye!");
+        return session;
+    }
+
+    /***
+     * Добавить инфу о занятом месте в БД
+     * @param session объект сеанса
+     * @param row ряд в зале
+     * @param seat место в зале
+     */
+    public void addSeat(Session session, int row, int seat) {
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        try {
+            //STEP 2: Register JDBC driver
+            Class.forName(JDBC_DRIVER);
+
+            //STEP 3: Open a connection
+            System.out.println("Connecting to database...");
+            conn = DriverManager.getConnection(DB_URL, USER, PASS);
+
+            //STEP 4: Execute a query
+            System.out.println("Creating statement...");
+
+            String sql;
+            sql = String.format("INSERT INTO %s ( %s, %s, %s ) VALUES ( ? , ? , ?   )",
+                    databases.seats,
+                    seatsFields.session_id,
+                    seatsFields.row,
+                    seatsFields.seat
+            );
+            stmt = conn.prepareStatement(sql);
+
+            stmt.setInt(1, session.getId());
+            stmt.setInt(2, row);
+            stmt.setInt(3, seat);
+            stmt.execute();
+
+
+            stmt.close();
+            conn.close();
+        } catch (Exception e) {
+            //Handle errors for Class.forName
+            e.printStackTrace();
+        } finally {
+            //finally block used to close resources
+            try {
+                if (stmt != null)
+                    stmt.close();
+            } catch (SQLException ignored) {
+            }// nothing we can do
+            try {
+                if (conn != null)
+                    conn.close();
+            } catch (SQLException se) {
+                se.printStackTrace();
+            }//end finally try
+        }//end try
+        System.out.println("Goodbye!");
 
     }
 
-
+    /**
+     * Вывести список всех залов(нужен ли)
+     * @return
+     */
     public List<Hall> retrieveHalls(){
         Connection conn = null;
         Statement stmt = null;
@@ -225,10 +322,10 @@ public class CinemaStorage {
                 result.add( new Hall(id,  name, descr, jsonObject ));
 
                 //Display values
-                System.out.print("ID: " + id);
-                System.out.print(", name: " + name);
-                System.out.print(", descr: " + descr);
-                System.out.println(", struct: " + struct);
+                //System.out.print("ID: " + id);
+                //System.out.print(", name: " + name);
+                //System.out.print(", descr: " + descr);
+                //System.out.println(", struct: " + struct);
             }
             //STEP 6: Clean-up environment
             rs.close();
@@ -257,10 +354,15 @@ public class CinemaStorage {
         System.out.println("Goodbye!");
         return result;
     }
+
+    /***
+     * Вывести список всех фильмов
+     * @return
+     */
     public List<Film> retrieveFilms(){
         Connection conn = null;
         Statement stmt = null;
-        ArrayList<Film>result=new ArrayList<Film>();
+        ArrayList<Film>result= new ArrayList<>();
         try{
             //STEP 2: Register JDBC driver
             Class.forName(JDBC_DRIVER);
@@ -292,9 +394,9 @@ public class CinemaStorage {
                 result.add( new Film(id,  name, jsonObject ));
 
                 //Display values
-                System.out.print("ID: " + id);
-                System.out.print(", name: " + name);
-                System.out.print(", descr: " + descr);
+                //System.out.print("ID: " + id);
+                //System.out.print(", name: " + name);
+                //System.out.print(", descr: " + descr);
             }
             //STEP 6: Clean-up environment
             rs.close();
@@ -324,11 +426,16 @@ public class CinemaStorage {
         return result;
 
     }
-    /*
-    public List<Session> retrieveSessions(String film_id){
+
+    /**
+     * Вывести список всех сеансов по названию фильма
+     * @param filmName название фильма
+     * @return
+     */
+    public List<Session> retrieveSessions(String filmName){
         Connection conn = null;
         PreparedStatement stmt = null;
-        ArrayList<Session>result=new ArrayList<Session>();
+        ArrayList<Session>result= new ArrayList<>();
         try{
             //STEP 2: Register JDBC driver
             Class.forName(JDBC_DRIVER);
@@ -342,31 +449,52 @@ public class CinemaStorage {
 
             String sql;
 
-            sql = String.format("SELECT %s, %s, %s, %s " +
-                            " FROM %s " +
-                            " WHERE %s=?",
-                    sessionFields.id,
-                    sessionFields.session_time,
-                    sessionFields.hall_id,
-                    sessionFields.price,
+            sql = String.format("SELECT a.%s, a.%s, a.%s, a.%s, a.%s," +
+                            " b.%s,b.%s,b.%s, " +
+                            " c.%s, c.%s, c.%s, c.%s"+
+                            " FROM %s a" +
+                            " LEFT JOIN %s b" +
+                            " ON (a.%s=b.%s) " +
+                            " LEFT JOIN %s c" +
+                            " ON (a.%s=c.%s) " +
+                            " WHERE b.%s=?",
+                    sessionFields.id, sessionFields.session_time, sessionFields.hall_id,sessionFields.film_id, sessionFields.price,
+                    filmFields.id,filmFields.name,filmFields.descr,
+                    hallFields.id,hallFields.name,hallFields.descr,hallFields.struct,
                     databases.session,
-                    sessionFields.film_id);
+                    databases.film,
+                    sessionFields.film_id,filmFields.id,
+                    databases.hall,
+                    sessionFields.hall_id,hallFields.id,
+                    filmFields.name);
             stmt = conn.prepareStatement(sql);
-            stmt.setString(1, film_id);
+            stmt.setString(1, filmName);
             ResultSet rs = stmt.executeQuery(sql);
 
             //STEP 5: Extract data from result set
             while(rs.next()){
                 //Retrieve by column name
                 int id  = rs.getInt( sessionFields.id.name() );
-                LocalDateTime session_time=LocalDateTime.parse(rs.getDate( sessionFields.session_time.name() ).toString() );
+                java.sql.Timestamp timestamp=rs.getTimestamp(sessionFields.session_time.name());
+                LocalDateTime session_time=timestamp.toLocalDateTime();
                 String hall_id = rs.getString( sessionFields.hall_id.name() );
                 int price = rs.getInt( sessionFields.price.name() );
 
-                result.add( new Session(id,session_time, hall_id, price));
+                int filmId=rs.getInt(filmFields.id.name());
+                String filmDescr=rs.getString(filmFields.descr.name());
+
+                Film film=new Film(filmId,filmName,getJSONObjectFromString(filmDescr));
+
+                int hallId=rs.getInt(hallFields.id.name());
+                String hallName=rs.getString(hallFields.name.name());
+                String hallDescr=rs.getString(hallFields.descr.name());
+                String hallStruct=rs.getString(hallFields.struct.name());
+
+                Hall hall=new Hall(hallId,hallName,hallDescr,getJSONObjectFromString(hallStruct));
+                result.add( new Session(id,film,hall,session_time, price));
 
                 //Display values
-                System.out.print("ID: " + id);
+                //System.out.print("ID: " + id);
 
             }
             //STEP 6: Clean-up environment
@@ -396,6 +524,7 @@ public class CinemaStorage {
         System.out.println("Goodbye!");
         return result;
     }
+    /*
     public List<Seat> retrieveSeats(){
 
     }*/
---
+++ server/src/test/java/ru/nstu/cinema/server/AddValuesToDb.java
@@ -0,0 +1,129 @@
+package ru.nstu.cinema.server;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import ru.nstu.cinema.common.entity.Film;
+import ru.nstu.cinema.common.entity.Hall;
+import ru.nstu.cinema.common.entity.Session;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+/**
+ * Created by ksuhan on 06.06.17.
+ */
+public class AddValuesToDb {
+    public static void main(String[] args) {
+        CinemaStorage cinemaStorage = new CinemaStorage();
+
+        Film film1 = new Film();
+        film1.setName("Wonder women");
+        film1.setDescr(new JSONObject() {{
+            put("country", "USA");
+            put("time", 217);
+        }});
+        film1=cinemaStorage.addFilm(film1);
+
+        Film film2 = new Film();
+        film2.setName("Harry Potter and the Sorcerer's Stone");
+        film2.setDescr(new JSONObject() {{
+            put("country", "USA");
+            put("time", 152);
+        }});
+        film2=cinemaStorage.addFilm(film2);
+
+        Hall hall1 = new Hall();
+        hall1.setName("Big hall");
+        hall1.setDescription("");
+        hall1.setStructure(new JSONObject() {{
+            JSONArray rows = new JSONArray();
+            for (int i = 0; i < 15; i++) {
+                final int id = i + 1;
+                rows.add(new JSONObject() {{
+                    put("id", id);
+                    put("seats", 15 - (id > 10 ? 0 : id > 5 ? 1 : 2));
+                }});
+            }
+            put("rows", rows);
+        }});
+        hall1=cinemaStorage.addHall(hall1);
+
+        Hall hall2 = new Hall();
+        hall2.setName("VIP hall");
+        hall2.setDescription("super hall");
+        hall2.setStructure(new JSONObject() {{
+            JSONArray rows = new JSONArray();
+            for (int i = 0; i < 15; i++) {
+                final int id = i + 1;
+                rows.add(new JSONObject() {{
+                    put("id", id);
+                    put("seats", 15 - (id > 10 ? 0 : id > 5 ? 1 : 2));
+                }});
+            }
+            put("rows", rows);
+        }});
+        hall2=cinemaStorage.addHall(hall2);
+
+        Hall hall3 = new Hall();
+        hall3.setName("Hall №1");
+        hall3.setDescription("Hall with super soft sofa");
+        hall3.setStructure(new JSONObject() {{
+            JSONArray rows = new JSONArray();
+            for (int i = 0; i < 15; i++) {
+                final int id = i + 1;
+                rows.add(new JSONObject() {{
+                    put("id", id);
+                    put("seats", 15 - (id > 10 ? 0 : id > 5 ? 1 : 2));
+                }});
+            }
+            put("rows", rows);
+        }});
+        hall3=cinemaStorage.addHall(hall3);
+
+        Session session = new Session();
+        session.setFilm(film1);
+        session.setHall(hall1);
+        session.setPrice(300);
+        session.setTime(LocalDateTime.of(2017, 6, 7, 15, 0));
+        cinemaStorage.addSession(session);
+
+        session = new Session();
+        session.setFilm(film1);
+        session.setHall(hall1);
+        session.setPrice(500);
+        session.setTime(LocalDateTime.of(2017, 6, 7, 21, 0));
+        cinemaStorage.addSession(session);
+
+        session = new Session();
+        session.setFilm(film2);
+        session.setHall(hall2);
+        session.setPrice(1500);
+        session.setTime(LocalDateTime.of(2017, 6, 7, 19, 0));
+        cinemaStorage.addSession(session);
+
+        session = new Session();
+        session.setFilm(film2);
+        session.setHall(hall2);
+        session.setPrice(2500);
+        session.setTime(LocalDateTime.of(2017, 6, 7,21 , 0));
+        cinemaStorage.addSession(session);
+
+        session = new Session();
+        session.setFilm(film2);
+        session.setHall(hall3);
+        session.setPrice(500);
+        session.setTime(LocalDateTime.of(2017, 6, 7,12 , 0));
+        cinemaStorage.addSession(session);
+
+
+        cinemaStorage.addSeat(session,10,1);
+
+        List<Film>films=cinemaStorage.retrieveFilms();
+        List<Hall>halls=cinemaStorage.retrieveHalls();
+        List<Session>sessions=cinemaStorage.retrieveSessions("Wonder woman");
+        for(Session l:sessions){
+            System.out.println(l.toString());
+        }
+
+    }
+}
